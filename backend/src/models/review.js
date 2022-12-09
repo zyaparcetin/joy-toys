27742ec077a51ccb6eb48baf34a07b87b7f8a4b4
zyaparcetin <lavinia.zeynep35@gmail.com>
2022-12-09 11:28:14 +0000

    Move my reviews.js

---
+++ backend/src/models/review.js
@@ -0,0 +1,18 @@
+// my review.js
+const mongoose = require('mongoose')
+const autopopulate = require('mongoose-autopopulate')
+
+const reviewSchema = new mongoose.Schema({
+  text: String,
+  rate: Number,
+  author: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      autopopulate: true,
+    },
+  ],
+})
+
+reviewSchema.plugin(autopopulate)
+module.exports = mongoose.model('Review', reviewSchema)

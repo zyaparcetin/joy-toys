c0831556935b79077c0696987f7d140a6c3fa53c
zyaparcetin <lavinia.zeynep35@gmail.com>
2022-12-09 11:47:11 +0000

    Move my users.js and comment out

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -1,33 +1,33 @@
-const express = require('express')
-const User = require('../models/user')
+// const express = require('express')
+// const User = require('../models/user')
+// const Product = require('../models/product')
 
-const router = express.Router()
+// const router = express.Router()
 
 /* GET users listing. */
-router.get('/', async (req, res) => {
+/* router.get('/', async (req, res) => {
   const query = {}
   res.send(await User.find(query).catch(error => console.log('Users not found, error: ', error)))
-})
+}) */
 
-/* GET initialize */
-router.get('/initialize', async (req, res) => {
-  const mihri = await User.create({ name: 'mihri', age: 35 })
-  const armagan = await User.create({ name: 'armagan', age: 36 })
+/* POST user */
+/* router.post('/', async (req, res) => {
+  const createdUser = await User.create(req.body)
+  res.status(201).send(createdUser)
+}) */
 
-  const steve = await User.create({ name: 'steve', age: 21 })
-  steve.bio = 'An awesome hacker who has seen it all, and now sharing them all with you.'
+// with userId
+/* router.get('/:userId', async (req, res) => {
+  const user = await User.findById(req.params.userId)
 
-  steve.greet(mihri)
-  steve.greet(armagan)
+  if (user) res.render('user', { user })
+  else res.sendStatus(404)
+}) */
 
-  console.log(steve)
+// to delete user with Id
+/* router.delete('/:userId', async (req, res) => {
+  await User.findByIdAndDelete(req.params.userId)
   res.sendStatus(200)
 })
 
-/* POST user */
-router.post('/', async (req, res) => {
-  const createdUser = await User.create(req.body)
-  res.status(201).send(createdUser)
-})
-
-module.exports = router
+module.exports = router */

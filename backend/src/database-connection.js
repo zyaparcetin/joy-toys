3422f8edb766c017c67e8943d58b8c36e12dd85e
zyaparcetin <lavinia.zeynep35@gmail.com>
2022-12-09 11:26:09 +0000

    Move from my project

--- backend/src/database-connection.js
+++ backend/src/database-connection.js
@@ -1,14 +1,13 @@
 const mongoose = require('mongoose')
 
 // eslint-disable-next-line prefer-const
-let connectionString = process.env.MONGODB_CONNECTION_STRING || 'mongodb://localhost:27017'
-
+let connectionString = process.env.MONGODB_CONNECTION_STRING || 'mongodb://mongo/joy-toys-local'
+console.log('connectionString', connectionString)
 mongoose.set('debug', true)
-
 mongoose
   .connect(connectionString, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log('connection established'))
+  .then(() => console.log('connection established', connectionString))
   .catch(error => console.log('not connected:', error))

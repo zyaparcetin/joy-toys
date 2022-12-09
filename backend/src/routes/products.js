2d5d69971cb77d5b33f22abf6cf001ddc1c0a663
zyaparcetin <lavinia.zeynep35@gmail.com>
2022-12-09 11:45:51 +0000

    Move my products.js and comment out

---
+++ backend/src/routes/products.js
@@ -0,0 +1,20 @@
+// const express = require('express')
+// const Product = require('../models/product')
+// const User = require('../models/user')
+// const Review = require('../models/review')
+
+// const router = express.Router()
+
+/* GET products listing. */
+/* router.get('/', async (req, res) => {
+  const query = {}
+  res.send(await Product.find(query).catch(error => console.log('Products not found, error: ', error)))
+}) */
+
+/* POST product */
+/* router.post('/', async (req, res) => {
+  const createdProduct = await Product.create(req.body)
+  res.status(201).send(createdProduct)
+})
+
+module.exports = router */

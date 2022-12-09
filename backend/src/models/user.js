9d3034778add007fc947cfc18ecab45a392b05b5
zyaparcetin <lavinia.zeynep35@gmail.com>
2022-12-09 11:28:52 +0000

    Move my user.js

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,5 +1,73 @@
 const mongoose = require('mongoose')
 const autopopulate = require('mongoose-autopopulate')
+const Review = require('./review')
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    // unique: true,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Product',
+      autopopulate: true,
+    },
+  ],
+  basket: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Product',
+      autopopulate: true,
+    },
+  ],
+  reviews: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Review',
+      autopopulate: true,
+    },
+  ],
+})
+
+class User {
+  greet() {
+    console.log(`Hello ${this.name}, welcome to Joy Toys!`)
+  }
+
+  async likeProduct(product) {
+    this.likes.push(product)
+    product.likedBy.push(this.name)
+    await this.save()
+    await product.save()
+  }
+
+  async addToBasket(product) {
+    this.basket.push(product)
+    await this.save()
+  }
+
+  async review(product, text, rate) {
+    const review = new Review(text, rate, this.name)
+    this.reviews.push(review)
+    product.reviews.push(review)
+    await this.save()
+    await review.save()
+  }
+}
+
+userSchema.loadClass(User)
+userSchema.plugin(autopopulate)
+
+module.exports = mongoose.model('User', userSchema)
+
+/* const mongoose = require('mongoose')
+const autopopulate = require('mongoose-autopopulate')
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -38,4 +106,4 @@ Bio: ${this.bio}
 userSchema.loadClass(User)
 userSchema.plugin(autopopulate)
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model('User', userSchema) */

77e17cfebda74e89fe07980c539586e3f7d3a7bc
zyaparcetin <lavinia.zeynep35@gmail.com>
2022-12-11 17:31:41 +0000

    Delete comment lines

--- backend/src/routes/products.js
+++ backend/src/routes/products.js
@@ -1,20 +1,20 @@
-// const express = require('express')
-// const Product = require('../models/product')
-// const User = require('../models/user')
-// const Review = require('../models/review')
+const express = require('express')
+const Product = require('../models/product')
+const User = require('../models/user')
+const Review = require('../models/review')
 
-// const router = express.Router()
+const router = express.Router()
 
 /* GET products listing. */
-/* router.get('/', async (req, res) => {
+router.get('/', async (req, res) => {
   const query = {}
   res.send(await Product.find(query).catch(error => console.log('Products not found, error: ', error)))
-}) */
+})
 
 /* POST product */
-/* router.post('/', async (req, res) => {
+router.post('/', async (req, res) => {
   const createdProduct = await Product.create(req.body)
   res.status(201).send(createdProduct)
 })
 
-module.exports = router */
+module.exports = router

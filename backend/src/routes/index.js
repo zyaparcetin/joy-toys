08fdd08e2c1a5ab21135921666ee8e15dbe629e4
zyaparcetin <lavinia.zeynep35@gmail.com>
2022-12-18 18:54:58 +0000

    Change and add some lines

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -11,8 +11,11 @@ router.get('/', (req, res) => {
 
 /* GET initialize */
 router.get('/initialize', async (req, res) => {
-  const Kerem = await User.create({ name: 'Kerem', age: 11 })
-  const Mete = await User.create({ name: 'Mete', age: 7 })
+  const Kerem = await User.create({ name: 'Kerem', age: 12, email: `kerem@example.com` })
+  const Mete = await User.create({ name: 'Mete', age: 7, email: `mete@example.com` })
+
+  Kerem.setPassword('test')
+  Mete.setPassword('test')
 
   const teddyBearProduct = await Product.create({ name: 'teddyBear', price: 19.99 })
   const kittyProduct = await Product.create({ name: 'kitty', price: 14.99 })
@@ -21,6 +24,9 @@ router.get('/initialize', async (req, res) => {
 
   await Kerem.likeProduct(teddyBearProduct)
   await Kerem.addToBasket(teddyBearProduct)
+
+  await Mete.likeProduct(frogletProduct)
+  await Mete.addToBasket(frogletProduct)
 
   console.log(Kerem)
   res.sendStatus(200)

8bade73b01322f40215b3a228060c526bec63874
zyaparcetin <lavinia.zeynep35@gmail.com>
2022-12-18 19:54:48 +0000

    Create product-card.vue

---
+++ frontend/src/components/icons/product-card.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  name: 'ProductCard',
+  props: ['product'],
+  data() {
+    return {}
+  },
+}
+</script>
+
+<template lang="pug">
+.box
+  h1 {{ product.name }} ({{ product.price }})
+ 
+</template>
+
+<style lang="scss" scoped>
+.box {
+  padding: 2rem;
+  border: 1px solid #333;
+  background: #dd0;
+  border-radius: 0.3rem;
+}
+</style>

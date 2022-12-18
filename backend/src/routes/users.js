7d16f4973e6c33beb33b872ebaca7dac24964f00
zyaparcetin <lavinia.zeynep35@gmail.com>
2022-12-18 18:39:40 +0000

    Return the previous users.js

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -10,29 +10,24 @@ router.get('/', async (req, res) => {
   res.send(await User.find(query).catch(error => console.log('Users not found, error: ', error)))
 })
 
-router.get('/initialize', async (req, res) => {
-  const yunus = new User({ username: 'Yunus', firstName: 'Yunus', lastName: 'Cetin', age: 38, email: 'y@gmail.com' })
-  await yunus.setPassword('test')
-  await yunus.save()
-})
 /* POST user */
-/* router.post('/', async (req, res) => {
+router.post('/', async (req, res) => {
   const createdUser = await User.create(req.body)
   res.status(201).send(createdUser)
 })
-*/
+
 // with userId
-/* router.get('/:userId', async (req, res) => {
+router.get('/:userId', async (req, res) => {
   const user = await User.findById(req.params.userId)
 
   if (user) res.render('user', { user })
   else res.sendStatus(404)
-}) */
+})
 
 // to delete user with Id
-/* router.delete('/:userId', async (req, res) => {
+router.delete('/:userId', async (req, res) => {
   await User.findByIdAndDelete(req.params.userId)
   res.sendStatus(200)
-}) */
+})
 
 module.exports = router

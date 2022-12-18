766cd53ee3c3276b07041bce3940cf55be2595fa
zyaparcetin <lavinia.zeynep35@gmail.com>
2022-12-18 15:01:10 +0000

    Change and add some lines

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,11 +4,16 @@ const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
-
-require('./database-connection')
-
 const cors = require('cors')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
+const mongoose = require('mongoose')
+const mongooseConnection = require('./database-connection')
+
+const clientPromise = mongoose.connection.asPromise().then(connection => connection.getClient())
+
 const indexRouter = require('./routes/index')
+// const accountRouter = require('./routes/account')
 const usersRouter = require('./routes/users')
 const productsRouter = require('./routes/products')
 
@@ -36,16 +41,34 @@ app.use(cors())
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
+app.use('/favicon.ico', express.static(path.join(__dirname, 'public', 'images', 'favicon.ico')))
+
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
+app.use(
+  session({
+    saveUninitialized: false,
+    resave: false,
+    // secret: [secret, validateSecret],
+    store: MongoStore.create({ clientPromise, stringify: false }),
+    cookie: {
+      maxAge: 14 * 24 * 60 * 60 * 1000,
+      path: '/api',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      secure: process.env.NODE_ENV === 'production',
+    },
+  })
+)
+
 app.use(express.static(path.join(__dirname, 'public')))
-app.use('/favicon.ico', express.static(path.join(__dirname, 'public', 'images', 'favicon.ico')))
 
 app.use('/api/', indexRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/products', productsRouter)
+// app.use('/api/account', accountRouter)
+app.use('/api/', indexRouter)
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {

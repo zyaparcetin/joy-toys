3983f8afe403e86e7b51bce9909efaba7c1de560
zyaparcetin <lavinia.zeynep35@gmail.com>
2022-12-18 13:16:16 +0000

    Change the 4th line

--- backend/src/database-connection.js
+++ backend/src/database-connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 // eslint-disable-next-line prefer-const
-let connectionString = process.env.MONGODB_CONNECTION_STRING || 'mongodb://mongo/joytoys'
+let connectionString = process.env.MONGODB_CONNECTION_STRING || 'mongodb://localhost:27017'
 console.log('connectionString', connectionString)
 mongoose.set('debug', true)
 mongoose

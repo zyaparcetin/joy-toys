f639f5c2c7db6c026acd1dfe593e7e3a167c5a18
zyaparcetin <lavinia.zeynep35@gmail.com>
2022-12-19 09:24:56 +0000

    Create product-detail.vue

---
+++ frontend/src/views/product-detail.vue
@@ -0,0 +1,25 @@
+<script>
+import ProductCard from '@/components/product-card.vue'
+import { mapActions } from 'vuex'
+export default {
+  name: 'ProductDetail',
+  components: { ProductCard },
+  data() {
+    return {
+      product: null,
+    }
+  },
+  async beforeMount() {
+    this.product = await this.fetchProduct(this.$route.params.id)
+  },
+  methods: {
+    ...mapActions(['fetchProduct']),
+  },
+}
+</script>
+
+<template lang="pug">
+  .about
+    ProductCard(:product="product" v-if="product" )
+    
+</template>

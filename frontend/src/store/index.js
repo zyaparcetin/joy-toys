d43cfe5304fa9d18a76805ae432898fbd1c03782
zyaparcetin <lavinia.zeynep35@gmail.com>
2022-12-19 11:04:02 +0000

    Change sth

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,64 +1,106 @@
+import axios from 'axios'
 import Vue from 'vue'
 import Vuex from 'vuex'
-import axios from 'axios'
+
 
 Vue.use(Vuex)
 
+const socket = io()
+
 const mutations = {
-  INCREMENT_COUNT: 'increment count',
   SET_USER: 'set user',
+  SET_PRODUCT: 'set product',
 }
 
 const store = new Vuex.Store({
   state: {
-    count: 0,
     user: null,
+    products: null,
+    reviews: null,
   },
   mutations: {
-    [mutations.INCREMENT_COUNT](state) {
-      state.count++
-    },
     [mutations.SET_USER](state, user) {
       state.user = user
     },
+    [mutations.SET_PRODUCT](state, products) {
+      state.products = products
+    },
+    /*setFilteredProducts( state, filteredProds) {
+      state.products= filteredProds      
+     }*/
   },
   actions: {
-    incrementCount({ commit }) {
-      commit(mutations.INCREMENT_COUNT)
-    },
+    //USER
     async fetchUser(store, id) {
-      const userRequest = await axios.get(`/api/users/${id}/json`)
+      const userRequest = await axios.get(`/api/users/${id}`)
       return userRequest.data
     },
     async fetchUsers() {
       const usersRequest = await axios.get('/api/users')
       return usersRequest.data
     },
-    async fetchSession({ commit }) {
-      const user = await axios.get('/api/account/session')
-      commit(mutations.SET_USER, user.data || null)
-    },
-    async login({ commit }, credentials) {
-      // eslint-disable-next-line no-useless-catch
-      try {
-        const user = await axios.post('/api/account/session', credentials)
-        commit(mutations.SET_USER, user.data)
-      } catch (e) {
-        throw e
-      }
-    },
-    async register(store, user) {
-      return axios.post('/api/account', user)
-    },
-    async logout({ commit }) {
-      await axios.delete('/api/account/session')
-      commit(mutations.SET_USER, null)
-    },
-  },
+    /*async fetchUserOrders(store, id) {
+      const userOrders = await axios.get(`/api/orders/user/${id}`)
+      return userOrders.data
+    },*/
+
+      // PRODUCT 
+  
+      /* async filterProducts(store, fPrice, FFuel, FGear) {
+        const filteredProduct = await axios.post('/api/products/filter', fPrice, FFuel, FGear)
+        return filteredProduct.data     
+      },
+      async fetchCompanyProducts(store, id) {
+        const companyProducts = await axios.get(`/api/products/company/${id}`)
+        return companyProducts.data
+      },
+      async fetchProducts({commit}) {
+        const productsRequest = await axios.get('/api/products')
+        commit(mutations.SET_PRODUCT, productsRequest.data)
+        return productsRequest.data
+      },
+      async fetchProduct(store, id) {
+        const productRequest = await axios.get(`/api/products/${id}`)
+        return productRequest.data
+      },
+      async addProduct(store, product) {
+        await axios.post('/api/products', product)
+      },
+      async editProduct(store, product) {
+        await axios.patch(`/api/products/update/${product.id}`, product)
+      },
+      async deleteProduct(store, id) {
+        return await axios.delete(`/api/products/${id}`)
+      },*/
+
+      async fetchSession({ commit }) {
+        const user = await axios.get('/api/account/session')
+        commit(mutations.SET_USER, user.data || null)
+        return user.data
+      },
+      async login({ commit }, credentials) {
+        try {
+          const user = await axios.post('/api/account/session', credentials)
+          commit(mutations.SET_USER, user.data)
+        } catch (e) {
+          throw e
+        }
+      },
+      async register(store, user) {
+        return axios.post('/api/account', user)
+      },
+      async logout({ commit }) {
+        await axios.delete('/api/account/session')
+        commit(mutations.SET_USER, null)
+      },
+
+    //REVIEW?
+    
   modules: {},
 })
 
 export default async function init() {
   await store.dispatch('fetchSession')
+  await store.dispatch('fetchProducts')
   return store
 }

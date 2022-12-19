1e7cc2a0552a15fae5b65f817e339f269dc67ccc
zyaparcetin <lavinia.zeynep35@gmail.com>
2022-12-19 09:11:49 +0000

    Change the name

--- frontend/src/views/login-view.vue
+++ frontend/src/views/login-view.vue
@@ -1,8 +1,9 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <script>
 import { mapActions } from 'vuex'
 
 export default {
-  name: 'login-view',
+  name: 'login',
   data() {
     return {
       email: '',

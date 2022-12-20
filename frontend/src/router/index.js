eb9eed28cef5afb2d4a709c71d469f13ee2d0aa6
zyaparcetin <lavinia.zeynep35@gmail.com>
2022-12-20 09:22:42 +0000

    Change index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,25 +1,50 @@
-/* eslint-disable no-undef */
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-// import Homepage from '../views/homepage.vue'  (projene göre eklemeler yap)
-import Login from '../views/login.vue'
-import Register from '../views/register.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/home-view.vue'
 
-Vue.use(VueRouter)
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    /*{
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/about-view.vue'),
+    },*/
+  ],
+})
+
+export default router
+
+//my project but doesn't work
+/* eslint-disable no-undef */
+//import Vue from 'vue'
+//import VueRouter from 'vue-router'
+// import Homepage from '../views/homepage.vue'  (projene göre eklemeler yap)
+//import Login from '../views/login.vue'
+//import Register from '../views/register.vue'
+
+//Vue.use(VueRouter)
 
 // Asagidaki kodu projene göre düzenle
-export default function init(store) {
+/*export default function init(store) {
   return new VueRouter({
     mode: 'history',
     base: process.env.BASE_URL,
-    routes: [
-      /*{
+    routes: [*/
+/*{
         path: '/',
         name: 'Homepage',
         component: Homepage,
       },*/
 
-      { path: '/products/:id', name: 'ProductDetail', component: () => import('../views/product-detail.vue') },
+/*{ path: '/products/:id', name: 'ProductDetail', component: () => import('../views/product-detail.vue') },
       {
         path: '/register',
         name: 'register',
@@ -40,4 +65,4 @@ export default function init(store) {
       },
     ],
   })
-}
+}*/

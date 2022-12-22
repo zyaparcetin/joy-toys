892cb5b73ab15a30a8818998f9bc5f4b7bdd04be
zyaparcetin <lavinia.zeynep35@gmail.com>
2022-12-22 14:22:17 +0000

    Add register path

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -9,6 +9,11 @@ const router = createRouter({
       name: 'home',
       component: HomeView,
     },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('../views/register.vue'),
+    },
     /*{
       path: '/about',
       name: 'about',

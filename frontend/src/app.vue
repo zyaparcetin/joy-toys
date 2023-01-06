13fa87523b67303b1d441b86ad96613a9b07b3b6
zyaparcetin <lavinia.zeynep35@gmail.com>
2023-01-06 16:18:59 +0000

    Delete style and some comment lines

--- frontend/src/app.vue
+++ frontend/src/app.vue
@@ -20,32 +20,5 @@ export default {
 #app
   #nav
     Navbar
-    //-router-link(to="/login") Login
-    //-router-link(to="/register") Register
-    //-a(@click="doLogout" href="#") Logout 
-    //-bir seyler eklemeli miyim mesela product-card?
   router-view
 </template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    margin: 0 1rem;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>

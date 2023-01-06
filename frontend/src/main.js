7917c0849a8fc790a87aa5797d4a5e9e8b59a34f
zyaparcetin <lavinia.zeynep35@gmail.com>
2023-01-06 16:19:22 +0000

    Comment out a line

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,9 @@
-import 'bootstrap'
+//import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.css'
 import { createApp } from 'vue'
 import App from './app.vue'
 import router from './router'
 import store from './store'
-
-import './assets/main.css'
 
 const app = createApp(App)
 

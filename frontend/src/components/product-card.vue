c40cf090bb6a6f3b07e028fb919a87e73237b254
zyaparcetin <lavinia.zeynep35@gmail.com>
2023-01-10 15:26:37 +0000

    Comment out some lines

--- frontend/src/components/product-card.vue
+++ frontend/src/components/product-card.vue
@@ -12,19 +12,19 @@ export default {
 .container
   .card-container
     .row
-      .col-5
-        img(:src="`${product.photo}`" width='100%' height='100%')
+      //- .col-5
+      //-   img(:src="`${product.photo}`" width='100%' height='100%')
       .col-7
         h3 {{ product.name }}
         h5 Product Price: {{ product.price }} €
-        button.btn.btn-primary(v-if="user" @click="buy(product)") Buy
-  .row.reviews Reviews
-    .col
-      h5(v-show="!product.revs.length") No reviews yet!
-      #product-reviews(v-for="review in product.revs")
-        h6 User name :  {{review.user.name}}
-        h6 Review : {{review.review}}
-        button.btn.btn-primary(v-if="review.user.email === user.email" @click="deleteRev(review)")  Delete review 
+        //- button.btn.btn-primary(v-if="user" @click="buy(product)") Buy
+  //- .row.reviews Reviews
+  //-   .col
+  //-     h5(v-show="!product.revs.length") No reviews yet!
+  //-     #product-reviews(v-for="review in product.revs")
+  //-       h6 User name :  {{review.user.name}}
+  //-       h6 Review : {{review.review}}
+  //-       button.btn.btn-primary(v-if="review.user.email === user.email" @click="deleteRev(review)")  Delete review 
 </template>
 
 <style lang="scss" scoped>

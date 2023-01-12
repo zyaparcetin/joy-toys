e787750d1d8673edd9134648616e91c87469215c
zyaparcetin <lavinia.zeynep35@gmail.com>
2023-01-12 20:02:42 +0000

    Add buy method and container

--- frontend/src/views/product-list.vue
+++ frontend/src/views/product-list.vue
@@ -10,6 +10,12 @@ export default {
   },
   methods: {
     ...mapActions(['fetchProducts']),
+    async buy() {
+      if (!this.user) {
+        alert('You need to Log-In to buy this product!')
+        this.$router.push('/login')
+      }
+    },
   },
   async created() {
     this.products = await this.fetchProducts()
@@ -18,18 +24,19 @@ export default {
 </script>
 
 <template lang="pug">  
-.row
-  h1 Our Products
-  //- h1 {{this.products}}
-  .col(v-if = "this.products === null")
-    h1 No Products 
-  .col
-    .card.col-12.col-sm-6.col-md-4.col-lg-3.mt-2(v-for="pr in this.products")
-      .card-body
-        router-link(:to="`/products/${pr._id}`")
-          h6.card-title {{ pr.name }}
-        h6.card-text Price : {{ pr.price }} € 
-        button.btn.btn-primary(v-if="user" @click="buy(pr)") Buy Now
+.container
+  .row
+    h1 Our Products
+    //- h1 {{this.products}}
+    .col(v-if = "this.products === null")
+      h1 No Products 
+    .col
+      .card.col-12.col-sm-6.col-md-4.col-lg-3.mt-2(v-for="pr in this.products")
+        .card-body
+          router-link(:to="`/products/${pr._id}`")
+            h6.card-title {{ pr.name }}
+          h6.card-text Price : {{ pr.price }} € 
+          button.btn.btn-primary( @click="buy(pr)") Buy Now
 </template>
 <style scoped>
 .card {

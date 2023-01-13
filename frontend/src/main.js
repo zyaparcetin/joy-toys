0cf5855fd36e02e46fb77f80b791113b0c91a981
zyaparcetin <lavinia.zeynep35@gmail.com>
2023-01-13 08:49:05 +0000

    Add 7th line to the main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,7 +4,7 @@ import { createApp } from 'vue'
 import App from './app.vue'
 import router from './router'
 import store from './store'
-
+store.dispatch('fetchSession')
 const app = createApp(App)
 
 app.use(router)

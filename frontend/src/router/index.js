01a5c1da1b1b30205f5c80a59634c4a9435a1bc1
zyaparcetin <lavinia.zeynep35@gmail.com>
2023-01-16 14:27:22 +0000

    Update index.js in router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -35,61 +35,12 @@ const router = createRouter({
         return next()
       },
     },
-
-    /*{
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/about-view.vue'),
-    },*/
+    /*  {
+      path: '/products/:id',
+      name: 'Detail',
+      component: () => import('../views/product-detail.vue'),
+    }, */
   ],
 })
 
 export default router
-
-//my project but doesn't work
-/* eslint-disable no-undef */
-//import Vue from 'vue'
-//import VueRouter from 'vue-router'
-// import Homepage from '../views/homepage.vue'  (projene göre eklemeler yap)
-//import Login from '../views/login.vue'
-//import Register from '../views/register.vue'
-
-//Vue.use(VueRouter)
-
-// Asagidaki kodu projene göre düzenle
-/*export default function init(store) {
-  return new VueRouter({
-    mode: 'history',
-    base: process.env.BASE_URL,
-    routes: [*/
-/*{
-        path: '/',
-        name: 'Homepage',
-        component: Homepage,
-      },*/
-
-/*{ path: '/products/:id', name: 'ProductDetail', component: () => import('../views/product-detail.vue') },
-      {
-        path: '/register',
-        name: 'register',
-        component: Register,
-        beforeEnter(to, from, next) {
-          if (store.state.user) return next('/profile')
-          return next()
-        },
-      },
-      {
-        path: '/login',
-        name: 'login',
-        component: Login,
-        beforeEnter(to, from, next) {
-          if (store.state.user) return next('/profile')
-          return next()
-        },
-      },
-    ],
-  })
-}*/

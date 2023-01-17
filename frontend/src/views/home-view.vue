9bff439acd3a7cd0ab434dce21ac71241bfc5fe2
zyaparcetin <lavinia.zeynep35@gmail.com>
2023-01-17 16:47:50 +0000

    Change the home page picture

--- frontend/src/views/home-view.vue
+++ frontend/src/views/home-view.vue
@@ -19,7 +19,8 @@ main.hero-image
 .hero-image {
   display: flex;
   flex-direction: column;
-  background-image: url('https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvczY2LWZvbi0wMDExLWxfMS5qcGc.jpg');
+  background-image: url('https://as1.ftcdn.net/v2/jpg/04/06/96/06/1000_F_406960680_62qFoiz279oVKe48yFCdmvq10F0hrcMo.jpg');
+  //https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvczY2LWZvbi0wMDExLWxfMS5qcGc.jpg
   //background-image: linear-gradient(rgba(0, 0, 0, 0.11), rgba(0, 0, 0, 0.11)), url('../assets/toys1.jpg');
   //https://as1.ftcdn.net/v2/jpg/04/06/96/06/1000_F_406960680_62qFoiz279oVKe48yFCdmvq10F0hrcMo.jpg
   // https://www.shutterstock.com/image-photo/frame-kids-toys-on-blue-600w-1500465974.jpg

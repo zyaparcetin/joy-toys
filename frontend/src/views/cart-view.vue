dbe1edf9ab60583e4c1ec7eea2fc1d2e8c6964d1
zyaparcetin <lavinia.zeynep35@gmail.com>
2023-01-24 10:13:41 +0000

    Update cart-view

--- frontend/src/views/cart-view.vue
+++ frontend/src/views/cart-view.vue
@@ -18,14 +18,45 @@ export default {
     .col(v-if = "user.cart === null")
       h1 No Items in Cart
     .col.product-list
-      
+  .row  
       .card.col-12.mt-2(v-for="product in user.cart")
         .col.img-container
           img.card-img-top.pt-1(:src="`${product.photo}`" :alt="`${product.name}`")
         .col
-          .card-body
-            //-router-link(:to="`/products/${product._id}`")
-              h6.card-title {{ product.name }}
+          .card-body   
+            h6 {{product.name}}    
             h6.card-text Price : {{ product.price }} € 
-            button.btn.btn-primary(@click="checkout") Checkout
+        .col-2
+          .btn-group.btn-group-lg
+            button.btn.btn-secondary -
+            button.btn.btn-secondary 1
+            button.btn.btn-secondary +
+        .col-2
+          button.btn.btn-danger(@click="deleteItem(product._id)") Delete
+
+//- .card.mb-3(style='max-width: 540px;')
+//- .row.g-0
+//-   h1 My Cart
+//-   .col-md-4(v-if = "user.cart === null")
+//-     h1 No Items in Cart
+//-     img.img-fluid.rounded-start(:src="`${product.photo}`" :alt="`${product.name}`")
+//-   .col-md-8
+//-     .card-body
+//-       h5.card-title Card title
+//-       p.card-text
+//-         | This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+//-       p.card-text
+//-         small.text-muted Last updated 3 mins ago
 </template>
+
+<style lang="scss" scoped>
+.card {
+  width: 35rem;
+  padding: 2rem;
+  border: 1px solid #333;
+  background: lightblue;
+  border-radius: 0.3rem;
+  margin: auto;
+  margin-top: 18px;
+}
+</style>

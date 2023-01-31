7f5182951a4ab93fde5d4d286f5ab3784f028864
zyaparcetin <lavinia.zeynep35@gmail.com>
2023-01-31 19:57:54 +0000

    Add a comment line in database con.

--- backend/src/database-connection.js
+++ backend/src/database-connection.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 
 // eslint-disable-next-line prefer-const
 let connectionString = process.env.MONGODB_CONNECTION_STRING || 'mongodb://localhost:27017'
+// process.env.MONGODB_CONNECTION_STRING ||'mongodb+srv://root:ryPc2s2vJo4W2PmL@cluster0.1se81sd.mongodb.net/joytoys?retryWrites=true&w=majority'
 console.log('connectionString', connectionString)
 mongoose.set('debug', true)
 mongoose
